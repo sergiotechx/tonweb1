1cf2d600378a3e4e1dda2fd0f9d515cc53c74d35
Sergio Martinez <sergiotechx@yahoo.com>
2024-10-10 17:10:20 -0500

    manifiest

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -2,8 +2,8 @@
 
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
-const manifestUrl = "https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json";
-
+//const manifestUrl = "https://ton-connect.github.io/demo-dapp-with-wallet/tonconnect-manifest.json";
+const manifestUrl = "https://tonweb-1-basic-features.vercel.app/tonconnect-manifest.json";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
